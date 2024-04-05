c1aaf6722117f3a452de1a3e66bcbe22001512ad
iremalkan <alkanirem212@gmail.com>
2024-04-05 12:18:54 +0300

    ASP.NET Core olarak guncellendi

---
+++ WebApp_razoryok/WebApp_razoryok/Controllers/projeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_razoryok.Controllers
+{
+    public class projeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
